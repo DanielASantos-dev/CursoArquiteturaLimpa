e5479f28689a607e861e7de7d5836b3cb79fb920
Daniel <qa.01@conlife.com.br>
2023-10-16 18:09:41 -0300

    aula 7

--- infrastructure/src/main/java/br/com/curso/repository/UserEntityRepository.java
+++ infrastructure/src/main/java/br/com/curso/repository/UserEntityRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserEntityRepository extends JpaRepository<UserEntity, UUID> {
-    Boolean existsByTaxNumber(String taxNumber);
 
-    Boolean existsByEmail(String email);
 }
